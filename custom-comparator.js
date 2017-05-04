c0087901f7609442586a935a5d71821dc71a8159
farnaz <farnaz.rashidi@gmail.com>
2017-05-04 20:21:28 +0000

    Add the custom-comparator.js file

---
+++ custom-comparator.js
@@ -0,0 +1,23 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+function comparator(obj) {
+  students.sort(function(a, b){
+      var nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase()
+      if (nameA < nameB) //sort string ascending
+          return -1
+      else if (nameA > nameB)
+          return 1
+      // comparing the ages if the names are the same
+      else {
+        return (a.age < b.age) ? -1 : 1
+      }
+  })
+  return students
+}
+
+console.log(comparator(students));
