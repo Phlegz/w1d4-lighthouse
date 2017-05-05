672a0365ddf53769c2644f7c12939aaeb5a408be
farnaz <farnaz.rashidi@gmail.com>
2017-05-05 03:11:01 +0000

    Modify the sum function so that the number of arguments it takes is not fixed

--- logging-wrapper.js
+++ logging-wrapper.js
@@ -2,13 +2,17 @@ var wrapLog = function (callback, name) {
   return function() {
     var result = callback.apply(null, arguments)
     var args = [...arguments]
-    return `${name}(${args[0]},${args[1]}) => ${result}`
+    return `${name} (${args})=> ${result}`
   }
 }
 
 //-------------------------------------------testing--------------------------
-var sum = function (a,b) { return a + b  }
+var sum = function () {
+  var args = [...arguments]
+  return args.reduce(function(a,b){return a+b})
+}
+
 var logSum = wrapLog(sum, "sum")
 
-console.log(logSum(5,3))  // sum(5,3) => 8
-console.log(logSum(3,2)) // sum(3,2) => 5
+console.log(logSum(5,3,4))  // sum(5,3,4) => 12
+console.log(logSum(3,2,0,-1,-2,-2)) // sum(3,2,0,-1,-2,-2) => 0
